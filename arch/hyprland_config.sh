69d8c13214d6062e03528f74f8b52bb389fc9b7a
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2025-07-04 15:01:23 +0600

    [UPDATE] cliphist added

--- arch/hyprland_config.sh
+++ arch/hyprland_config.sh
@@ -6,10 +6,12 @@ sudo pacman -Sy --noconfirm --needed hyprland
 echo "[INSTALL] hyprpaper hyprshot hyprlock swaync"
 sudo pacman -Sy --noconfirm --needed hyprpaper hyprshot hyprlock swaync
 
-
 echo "[INSTALL] ddcutil"
 sudo pacman -Sy --noconfirm --needed ddcutil
 sudo usermod -aG i2c $USER
+
+echo "[INSTALL] cliphist"
+sudo pacman -Sy --noconfirm --needed cliphist
 
 echo "[CONFIG] Hyprland config"
 mkdir -p ~/.config/hypr
