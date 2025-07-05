19b06d65e3b6aeba545041329e0daa8a418fa592
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2025-07-06 01:39:08 +0600

    [UPDATE] lualine realtive path, keymaps improved for which-key

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,71 +1,160 @@
--- Set leader key
+-- ============================================================================
+-- NEOVIM KEYMAPS CONFIGURATION
+-- ============================================================================
+
+-- Set leader key to spacebar for custom key combinations
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Disable the spacebar key's default behavior in Normal and Visual modes
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+-- This prevents conflicts with our leader key mappings
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true, desc = "Disable spacebar" })
 
--- For conciseness
-local opts = { noremap = true, silent = true }
+-- ============================================================================
+-- FILE OPERATIONS
+-- ============================================================================
 
--- save file
-vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+-- Save current file using Ctrl+S
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", { noremap = true, silent = true, desc = "Save file" })
 
--- save file without auto-formatting
-vim.keymap.set("n", "<leader>sn", "<cmd>noautocmd w <CR>", opts)
+-- Save file without running auto-formatting commands
+vim.keymap.set(
+	"n",
+	"<leader>sn",
+	"<cmd>noautocmd w <CR>",
+	{ noremap = true, silent = true, desc = "Save without formatting" }
+)
 
--- quit file
-vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
+-- Quit current file/buffer using Ctrl+Q
+vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", { noremap = true, silent = true, desc = "Quit file" })
 
--- delete single character without copying into register
-vim.keymap.set("n", "x", '"_x', opts)
+-- ============================================================================
+-- EDITING ENHANCEMENTS
+-- ============================================================================
 
--- Vertical scroll and center
-vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
-vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+-- Delete single character without copying it to the register (clipboard)
+-- Useful to avoid polluting your clipboard with single characters
+vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true, desc = "Delete char without yanking" })
 
--- Find and center
-vim.keymap.set("n", "n", "nzzzv", opts)
-vim.keymap.set("n", "N", "Nzzzv", opts)
+-- Vertical scroll half page down and center cursor on screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true, desc = "Scroll down and center" })
 
--- Resize with arrows
-vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<Right>", ":vertical resize +2<CR>", opts)
+-- Vertical scroll half page up and center cursor on screen
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true, desc = "Scroll up and center" })
 
--- Buffers
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
-vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
+-- Find next search result and center it on screen with cursor in middle
+vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true, desc = "Next search result" })
 
--- Window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
+-- Find previous search result and center it on screen with cursor in middle
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true, desc = "Previous search result" })
 
--- Navigate between splits
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
+-- ============================================================================
+-- WINDOW RESIZING
+-- ============================================================================
 
--- Tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab
+-- Decrease window height by 2 lines using Up arrow
+vim.keymap.set("n", "<Up>", ":resize -2<CR>", { noremap = true, silent = true, desc = "Decrease window height" })
 
--- Toggle line wrapping
-vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
+-- Increase window height by 2 lines using Down arrow
+vim.keymap.set("n", "<Down>", ":resize +2<CR>", { noremap = true, silent = true, desc = "Increase window height" })
 
--- Stay in indent mode
-vim.keymap.set("v", "<", "<gv", opts)
-vim.keymap.set("v", ">", ">gv", opts)
+-- Decrease window width by 2 columns using Left arrow
+vim.keymap.set(
+	"n",
+	"<Left>",
+	":vertical resize -2<CR>",
+	{ noremap = true, silent = true, desc = "Decrease window width" }
+)
 
--- Keep last yanked when pasting
-vim.keymap.set("v", "p", '"_dP', opts)
+-- Increase window width by 2 columns using Right arrow
+vim.keymap.set(
+	"n",
+	"<Right>",
+	":vertical resize +2<CR>",
+	{ noremap = true, silent = true, desc = "Increase window width" }
+)
 
--- Diagnostic keymaps
+-- ============================================================================
+-- BUFFER NAVIGATION
+-- ============================================================================
+
+-- Switch to next buffer in the buffer list
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+
+-- Switch to previous buffer in the buffer list
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
+
+-- Force close current buffer (discards unsaved changes)
+vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", { noremap = true, silent = true, desc = "Close buffer" })
+
+-- Create a new empty buffer
+vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", { noremap = true, silent = true, desc = "New buffer" })
+
+-- ============================================================================
+-- WINDOW MANAGEMENT
+-- ============================================================================
+
+-- Split current window vertically (creates new window to the right)
+vim.keymap.set("n", "<leader>v", "<C-w>v", { noremap = true, silent = true, desc = "Split window vertically" })
+
+-- Split current window horizontally (creates new window below)
+vim.keymap.set("n", "<leader>h", "<C-w>s", { noremap = true, silent = true, desc = "Split window horizontally" })
+
+-- Make all split windows equal width and height
+vim.keymap.set("n", "<leader>se", "<C-w>=", { noremap = true, silent = true, desc = "Make splits equal" })
+
+-- Close current split window
+vim.keymap.set("n", "<leader>xs", ":close<CR>", { noremap = true, silent = true, desc = "Close split" })
+
+-- ============================================================================
+-- SPLIT WINDOW NAVIGATION
+-- ============================================================================
+
+-- Move to window above current one
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { noremap = true, silent = true, desc = "Move to window above" })
+
+-- Move to window below current one
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { noremap = true, silent = true, desc = "Move to window below" })
+
+-- Move to window to the left of current one
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true, desc = "Move to window left" })
+
+-- Move to window to the right of current one
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { noremap = true, silent = true, desc = "Move to window right" })
+
+-- ============================================================================
+-- TAB MANAGEMENT
+-- ============================================================================
+
+-- Open a new tab
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", { noremap = true, silent = true, desc = "Open new tab" })
+
+-- Close current tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", { noremap = true, silent = true, desc = "Close tab" })
+
+-- Go to next tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>", { noremap = true, silent = true, desc = "Next tab" })
+
+-- Go to previous tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", { noremap = true, silent = true, desc = "Previous tab" })
+
+-- ============================================================================
+-- DISPLAY OPTIONS
+-- ============================================================================
+
+-- Toggle line wrapping on/off for long lines
+vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", { noremap = true, silent = true, desc = "Toggle line wrap" })
+
+-- ============================================================================
+-- VISUAL MODE ENHANCEMENTS
+-- ============================================================================
+
+-- Stay in indent mode after indenting left - allows for multiple indentations
+vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true, desc = "Indent left" })
+
+-- Stay in indent mode after indenting right - allows for multiple indentations
+vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true, desc = "Indent right" })
+
+-- Paste over selected text without losing the original yanked content
+-- This prevents the selected text from replacing your clipboard content
+vim.keymap.set("v", "p", '"_dP', { noremap = true, silent = true, desc = "Paste without yanking" })
--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -14,6 +14,7 @@ return {
 					nvimtree = true,
 					telescope = true,
 					treesitter = true,
+					markdown = true,
 				},
 			})
 			vim.cmd.colorscheme("catppuccin")
