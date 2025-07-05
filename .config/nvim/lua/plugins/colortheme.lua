414dc03e09b4a156fff05372aff07cc6cd1e5ddf
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2025-07-05 21:05:20 +0600

    [UPDATE] autocompletion working

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,20 +1,22 @@
 return {
-	"catppuccin/nvim",
-	name = "catppucin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = false,
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				nvimtree = true,
-				telescope = true,
-				treesitter = true,
-			},
-		})
-		vim.cmd.colorscheme("catppuccin")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppucin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = false,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					telescope = true,
+					treesitter = true,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
