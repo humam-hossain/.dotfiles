3f79fb9f2d9ee1a02359b42261fcc7926d265cab
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2025-07-07 12:34:59 +0600

    [FIX] vim-tpipeline double line issue solved

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,82 +1,41 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "lewis6991/gitsigns.nvim" },
-	event = "VeryLazy",
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
+	{
+		"vimpostor/vim-tpipeline",
+		event = "VimEnter",
+		config = function()
+			vim.g.tpipeline_autoembed = 1
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		event = "VeryLazy",
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+					section_seperator = "",
+					component_seperators = "",
+					icons_enabled = true,
+					globalstatus = false,
 				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				always_show_tabline = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
+				dependencies = { "nvim-tree/nvim-web-devicons" },
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { {
+						"filename",
+						path = 1,
+					} },
+					lualine_x = { "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
 				},
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = {
-					{
-						"branch",
-						fmt = function(str)
-							if str == "" or str == nil then
-								return "No Git"
-							end
-							return str
-						end,
-					},
-					{
-						"diff",
-						colored = true,
-						diff_color = {
-							added = "DiffAdd",
-							modified = "DiffChange",
-							removed = "DiffDelete",
-						},
-						symbols = { added = "+", modified = "~", removed = "-" },
-						source = function()
-							local gitsigns = vim.b.gitsigns_status_dict
-							if gitsigns then
-								return {
-									added = gitsigns.added,
-									modified = gitsigns.changed,
-									removed = gitsigns.removed,
-								}
-							end
-						end,
-					},
-					"diagnostics",
-				},
-				lualine_c = { {
-					"filename",
-					path = 1,
-				} },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
-		})
-	end,
+			})
+			vim.o.laststatus = 0
+		end,
+	},
 }
