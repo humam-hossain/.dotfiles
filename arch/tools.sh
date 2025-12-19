dfc5eae040168fc49b537bd19da17af1e74c9052
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2025-12-19 21:39:07 +0600

    update: catppuccin theme

--- arch/tools.sh
+++ arch/tools.sh
@@ -1,10 +1,5 @@
 set -xe
 
-echo "[INSTALL] btop htop"
-sudo pacman -Sy --noconfirm --needed btop htop
-mkdir -p ~/.config/btop/
-cp -rf .config/btop/* ~/.config/btop/
-
 echo "[INSTALL] unzip & tar"
 sudo pacman -Sy --noconfirm --needed unzip tar
 
