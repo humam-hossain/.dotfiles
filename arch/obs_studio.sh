400a6e8d282684c3aac0fbb250dd1de1fffcbbfc
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2026-02-09 15:44:50 +0600

    [UPDATE] obs studio setup

--- arch/obs_studio.sh
+++ arch/obs_studio.sh
@@ -1,6 +1,11 @@
 set -xe
 
-echo "[INFO] install obs studio"
-sudo pacman -Sy --noconfirm --needed obs-studio pipewire wireplumber xdg-desktop-portal-wlr
+echo "[INSTALL] obs studio"
+sudo pacman -Sy --noconfirm --needed obs-studio qt6-wayland v4l2loopback-dkms
 
-echo ""
+echo "[INSTALL] phn as webcam"
+yay -Sy --noconfirm --needed droidcam
+
+echo "[CONFIG] load the kernel module so Zoom can "see" the phone as a webcam device"
+sudo modprobe videodev
+sudo modprobe v4l2loopback-dc
