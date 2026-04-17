a631cee3811963d433575dc325301bac583d6492
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2026-04-17 20:41:37 +0600

    fix(09): disable render-markdown in nofile buffers to prevent treesitter crash
    render-markdown activates on picker preview buffers (buftype=nofile) and
    triggers treesitter injection parsing which hits a nil node bug, crashing
    the global highlighter. Disable it for nofile buftype.

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -63,7 +63,10 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you use the mini.nvim suite
-		opts = {},
+		opts = {
+			-- Disable in nofile buffers (snacks picker previews) to avoid treesitter nil node crash
+			buftype = { nofile = { enabled = false } },
+		},
 	},
 	{
 		"mluders/comfy-line-numbers.nvim",
