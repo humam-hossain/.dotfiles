97773584d3cc74decb5cbe45256fbd5b6d480a06
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2026-09-15 00:18:20 +0600

    feat(fish): configure starship, terminal colors, and aliases

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,12 +1,39 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    function starship_transient_prompt_func
+        starship module character
+    end
+    if test "$TERM" != "linux"
+        starship init fish | source
+        enable_transience
+    end
+    
+    # Colors
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    # Aliases
+    # kitty doesn't clear properly so we need to do this weird printing
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias celar "printf '\033[2J\033[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[3J\033[1;1H'"
+    alias pamcan pacman
+    alias q 'qs -c ii'
+    if test "$TERM" != "linux"
+        alias ls 'eza --icons=auto'
+    end
+    if test "$TERM" = "xterm-kitty"
+        alias ssh 'kitten ssh'
+    end
 end
 
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /home/pera/.lmstudio/bin
 # End of LM Studio CLI section
-
-
 
 # Added by Antigravity CLI installer
 set -gx PATH "/home/pera/.local/bin" $PATH
